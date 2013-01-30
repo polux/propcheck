de09d06049ffd681acad20220ba81d2e99fa7b50
Paul Brauner <polux@google.com>
2013-01-30 11:18:24 +0100

    simplify demo

--- example/demo.dart
+++ example/demo.dart
@@ -5,6 +5,7 @@
 
 library demo;
 
+import 'dart:collection';
 import 'package:propcheck/propcheck.dart';
 import 'package:enumerators/combinators.dart' as c;
 import 'package:unittest/unittest.dart';
@@ -12,34 +13,26 @@ import 'package:unittest/unittest.dart';
 // Defines append and reverse.
 part 'demolib.dart';
 
-// We turn dart unittest's listEquals into a boolean function.
-bool listEquals(List xs, List ys) {
-  try { Expect.listEquals(xs, ys); return true; }
-  catch (_) { return false; }
-}
+/* --- The properties to test --- */
 
 // This should hold for any sound implementation of reverse and append.
 bool good(List xs, List ys) =>
-  listEquals(reverse(append(xs, ys)),
-             append(reverse(ys), reverse(xs)));
+    Arrays.areEqual(reverse(append(xs, ys)),
+                    append(reverse(ys), reverse(xs)));
 
 // This should NOT hold for any sound implementation of reverse and append.
-// We usually don't want to test false properties, it is false for the sake of
-// the presentation.
 bool bad(List xs, List ys) =>
-    listEquals(reverse(append(xs, ys)),
-               append(reverse(xs), reverse(ys)));
+    Arrays.areEqual(reverse(append(xs, ys)),
+                    append(reverse(xs), reverse(ys)));
+
+/* --- How we test them --- */
 
 main() {
-  // We define an enumeration of lists of booleans.
+  // We define an enumeration of lists of integers.
   final boolsLists = c.listsOf(c.bools);
 
-  // We declare the properties '∀x:List<bool>, ∀y:List<bool>, good(x,y)' and
-  // '∀x:List<bool>, ∀y:List<bool>, bad(x,y)'. We use 'forall2' because 'good'
-  // and 'bad' expect *two* arguments each. In 'goodProperty', the arguments of
-  // 'good' are made explicit to hilight the similarity with the formula we
-  // wish to represent, while 'badProperty' features a terser style.
-  Property goodProperty = forall2(boolsLists, boolsLists, (x,y) => good(x,y));
+  // 'good' and 'bad' take 2 arguments each so we use forall2.
+  Property goodProperty = forall2(boolsLists, boolsLists, good);
   Property badProperty = forall2(boolsLists, boolsLists, bad);
 
   // We test the properties against *every* pair of lists of bools whose
