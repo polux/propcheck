faaf661b77e636271a0613fe9ad01b3580836e97
Paul Brauner <polux@google.com>
2014-05-31 22:57:29 +0200

    added mirror tests to the test script

--- tool/run_all_tests.sh
+++ tool/run_all_tests.sh
@@ -6,4 +6,5 @@ dartanalyzer $ROOT_DIR/lib/*.dart \
 && dartanalyzer $ROOT_DIR/test/*.dart \
 && dartanalyzer $ROOT_DIR/example/*.dart \
 && dart --enable-checked-mode $ROOT_DIR/test/smallcheck_test.dart \
-&& dart --enable-checked-mode $ROOT_DIR/test/quickcheck_test.dart
+&& dart --enable-checked-mode $ROOT_DIR/test/quickcheck_test.dart \
+&& dart --enable-checked-mode $ROOT_DIR/test/mirrors_test.dart
